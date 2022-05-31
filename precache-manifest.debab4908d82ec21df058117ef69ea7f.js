ccf1207a22550f892abb984992f7bd092492abf4
yasmimmade <yasmimmade@users.noreply.github.com>
2022-05-31 19:41:01 +0000

    deploy: 67876cd73963ceb9683171c8fe4e43e153a30ac0

---
+++ precache-manifest.debab4908d82ec21df058117ef69ea7f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7f8a3612059890d107cb900fae7f255",
+    "url": "https://yasmimmade.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "d32c7c73e42197b5cdf0",
+    "url": "https://yasmimmade.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "a31bfb9bafd74c75023b",
+    "url": "https://yasmimmade.github.io/task-management-frontend/static/js/2.2522fd50.chunk.js"
+  },
+  {
+    "revision": "d32c7c73e42197b5cdf0",
+    "url": "https://yasmimmade.github.io/task-management-frontend/static/js/main.5dbcf4a4.chunk.js"
+  },
+  {
+    "revision": "8aea5f3989d88a996b43",
+    "url": "https://yasmimmade.github.io/task-management-frontend/static/js/runtime~main.4d3fa75b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://arielweinberger.github.io/task-management-frontend/precache-manifest.0a88baa7b4ebb92ea89c64cd3e3f4832.js"
+  "https://yasmimmade.github.io/task-management-frontend/precache-manifest.debab4908d82ec21df058117ef69ea7f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://arielweinberger.github.io/task-management-frontend/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://yasmimmade.github.io/task-management-frontend/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
